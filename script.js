cf21e5744a165d953bb6a70550f1d66e6d35ebaf
steppanyaki <a0930794927@gmail.com>
2016-10-05 23:42:50 -0400

    Test image jquery mobile

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ $(document).ready(function() {
        $(this).fadeTo('fast',.01); 
     });
 });
+
+$(document).ready(function() {
+    $('.words').on('touchstart', function() {
+        $(this).fadeTo('fast', .95)
+    });
+    $('.words').mouseleave(function() {
+       $(this).fadeTo('fast',.01); 
+    });
+});
