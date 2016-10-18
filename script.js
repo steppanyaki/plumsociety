537e75ed2e5c2923814238049748e04a6f1a80e6
steppanyaki <a0930794927@gmail.com>
2016-10-18 18:30:33 -0400

    update website with super girl

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 $(document).ready(function() {
-    $('.words').mouseenter(function() {
+    $(".words").mouseenter(function() {
         $(this).fadeTo('fast', .95)
     });
-    $('.words').mouseleave(function() {
-       $(this).fadeTo('fast',.01); 
-    });
-});
-
-$(document).ready(function() {
-    $('.words').on('touchstart', function() {
-        $(this).fadeTo('fast', .95)
-    });
-    $('.words').mouseleave(function() {
+    $(".words").mouseleave(function() {
        $(this).fadeTo('fast',.01); 
     });
 });
